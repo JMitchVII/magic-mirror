16bf5757510232b3a9ad412cac4e2152d31a5ca6
jmitchell <jmitchell@nla.gov.au>
2020-02-12 22:47:58 +1100

    Adds a ROB

---
+++ New folder/ROB.js
@@ -0,0 +1,55 @@
+var bind = Function.prototype.bind,
+    $append = bind.call(Element.prototype.appendChild, document.querySelector("output")),
+    $new = bind.call(Document.prototype.createElement, document),
+    $text = bind.call(Document.prototype.createTextNode, document),
+    $rnd = function() { return (Math.random() * 125 + 0)|0; }, 
+    $promise = function(thenFn) {
+      var args, promise, wait, slice=Array.prototype.slice, isResolved = false;
+      var promise = {
+        wait: function(ms) {
+          wait = ms;
+          return promise;
+        },
+        then: function() {
+          args = slice.call(arguments);
+          return promise = $promise(thenFn);
+        },
+        resolve: function() {
+          isResolved = true;
+          if(args) {
+            var next = Function.prototype.bind.apply(thenFn, [undefined].concat(args).concat([promise]));
+            wait ? setTimeout(next, wait) : next();
+          }
+
+        }
+      };
+      return promise;
+    };
+
+var process = function(target, chars, promise) {
+  var first = chars[0], rest = chars.slice(1);
+  if(!first) {
+    promise.resolve();
+    return;
+  }
+  target.appendChild(first);
+  setTimeout(process.bind(undefined, target, rest, promise), $rnd());
+}
+
+var type = function(text, promise) {
+  var chars = text.split("").map($text);
+  promise = promise || $promise(type);
+  $append($new("br"));
+  process($append($new("q")), chars, promise);
+  return promise;
+};
+
+ type("Rob, Rob never changes..")
+  .wait(500)
+  .then("UwU").wait(500)
+  .then("Trust Vault-tec")
+  .wait(1500)
+  .then("Hey").wait(500)
+  .then("Hey you.").wait(1500)
+ .then("Listen, HEY!").wait(1500)
+  .then("You're a legend!");
